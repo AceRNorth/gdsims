d2c88d4e7f285731481cb15e0b22447ffd54e700
sprzv <s.perez.vizan@gmail.com>
2023-06-15 13:41:03 +0100

    Clean out of code - improved readability

--- headGen.h
+++ headGen.h
@@ -16,46 +16,85 @@
 //#include <random>
 #include <chrono>
 #include <algorithm>
-			
-/*-----------------------------------------------------Header definitions---------------------------------------------------------*/
-/*--------------------------------------------------------------------------------------------------------------------------------*/
-
-
-const int TL=20;//juvenile development time (egg to adult)
-const int NumGen=6;
 
 using namespace std;
 //using std::ifstream;
 using namespace std::tr1;
 
-const double PI=3.14159265;
-const double TWOPI=6.28318531;
-const long long int LONG_MAX=3147483647000;
+/*-----------------------------------------------------Header definitions---------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------*/
+
+
+const int TL = 20; //juvenile development time (egg to adult)
+const int NumGen = 6;
+
+const double PI = 3.14159265;
+const double TWOPI = 6.28318531;
+// const long long int LONG_MAX=3147483647000;  // had error - figure what's going wrong
+
+/*-------------------------------function declarations-------------------------------------*/
+void RunMaxT();
+void RunNReps(int N);
+void initiate(); // set up the simulation sites and variables
+void SitesPopulate(int pat); // initiate populations in the sites
+void record(int index); // output data from each individual site
+void PutDriverSites(int pat); // release gene drives into population
+void UpdateConnec(); // computes inter-patch connectivities
+void OneStep(int day); // control population processes
+
+/*--------- population processes (controlled from OneStep)-----*/
+void JuvGetOlder();
+void VirginsMate();
+void AdultsMove();
+void Hide();
+void Wake(int day);
+void LayEggs();
+void JuvEmerge();
+void AdultsDie();
+void UpdateComp(int day);
+void UpdateMate();
+/*-------------------------------------------------------------------*/
+void SetFertility();
+void CheckCounts(int TT, char ref); // function for debugging - checks if totals are the same as the number in each class in each population
+
+/*--------random number generater functions-----------------------*/
+long long int random_poisson(double landa);
+long long int random_binomial(long long int N, double p);
+int IRandom(int a, int b);
+double Random();
+// double random_normal(double,double);
+std::vector<int> random_Multinomial(int N, const std::vector<double>& probs);
+/*-------------------------------------------------------------------*/
+
+double distance(double U, double x1, double y1, double x2, double y2); // distance between points (assuming toroidal boundaries)
+double abso(double XX); // absolute used in distance function
+
+/*----------------------------------------------------------------------------------------*/
+
 
 
 /*----------------------struct for keeping track of global numbers------------------------*/
-struct totals
-	{
-	
+struct totals {
 	long long int J[NumGen];
 	long long int M[NumGen];
 	long long int V[NumGen];
 	long long int F[NumGen];
 	long long int CentF[NumGen];
-	long long int JTot,MTot,VTot,FTot;
+	long long int JTot, MTot, VTot, FTot;
 	int CentSqVils;
 	double CentSqHum;
-	};	
+};	
 /*----------------------------------------------------------------------------------------*/
 
 
 /*----------------------struct combining initial condition parameters---------------------*/
-struct initials
-{
-	int NumAdultsWM; int NumAdultsWV; int NumAdultsWF;
-	int NumAdultsDM; int NumAdultsDV; int NumAdultsDF;
-	int NumAdultsRM; int NumAdultsRV; int NumAdultsRF;
-	int NumJW[TL]; int NumJD[TL]; int NumJR[TL];
+struct initials {
+	int NumAdultsWM, NumAdultsWV, NumAdultsWF;
+	int NumAdultsDM, NumAdultsDV, NumAdultsDF;
+	int NumAdultsRM, NumAdultsRV, NumAdultsRF;
+	int NumJW[TL];
+	int NumJD[TL];
+	int NumJR[TL];
 	double driver_time;
 	double driver_start;
 	double driver_end;
@@ -67,13 +106,12 @@ struct initials
 	int NumR;
 	int NumRes;
 	int recSitesFreq;
-	};	
+};	
 /*----------------------------------------------------------------------------------------*/
 
 		
 /*------------------------state of population in each settlement---------------------------*/
-struct Patch
-{
+struct Patch {
 	double x;
 	double y;
 //	double Nhum;
@@ -82,7 +120,7 @@ struct Patch
 	double fun;
 	double area;
 	double dens;
-	int sqx,sqy;
+	int sqx, sqy;
 	int numsites;
 	long long int J[NumGen][TL];
 	long long int JTot;
@@ -99,11 +137,10 @@ struct Patch
 
 
 /*----- for determining connectivities between patches-------*/
-	vector<int> connecIND; //connection indices
-	vector<double> connecW;// connection weights
+	std::vector<int> connecIND; //connection indices
+	std::vector<double> connecW;// connection weights
 	double TotW;// sum of connection weights
 /*-----------------------------------------------------------*/
-
 	int CentSq;
 };
 /*----------------------------------------------------------------------------------------*/
@@ -111,8 +148,7 @@ struct Patch
 
 
 /*----------------struct containt simulation timekeeping parameters-------------------------*/
-struct Times
-{
+struct Times {
 	int maxT;// maximum simulated time
 	int start;
 	int recfreq; //record frequency
@@ -122,65 +158,39 @@ struct Times
 	int interval;// how often to output global variables
 };
 
-
-
 /*----------------------------------------------------------------------------------------*/
 
 /*---------------------struct containing model parameters----------------------------------*/
-struct Pars
-	{
-	double muJ,muA,d,gamma,beta,theta,xi,e,em,ef,dL,muLD,psi,muAES;
-	double LD;// maximum distance at which two sites are connected
+struct Pars {
+	double muJ;
+	double muA;
+	double d;
+	double gamma;
+	double beta;
+	double theta;
+	double xi;
+	double e;
+	double em;
+	double ef;
+	double dL;
+	double muLD;
+	double psi;
+	double muAES;
+	double LD; // maximum distance at which two sites are connected
 	double alpha0;
 	double LarvProbs[TL];
 	double meanTL;
-	double U;// size of simulation area (side)
-	double CentRad;//radius of interior area of simulation
-	int NumPat;// number of population sites
-	int set,index;// for labelling the output files
+	double U; // size of simulation area (side)
+	double CentRad; //radius of interior area of simulation
+	int NumPat; // number of population sites
+	int set, index; // for labelling the output files
+
 	/*----- aestivation parameters------------*/
 	int t_hide1,t_hide2,t_wake1,t_wake2;
 	/*----------------------------------------*/
-	double f[NumGen][NumGen][NumGen];//f_ijk is fraction of type k offspring from mother with genotype i mated to father with genotype j
-	};
-/*----------------------------------------------------------------------------------------*/
 
-/*-------------------------------function declarations-------------------------------------*/
-		void RunMaxT();
-		void RunNReps(int);
-		void ResetSites();
-		void initiate();// set up the simulation sites and variables
-		void SitesPopulate(int);// initiate populations in the sites
-		void record(int);// output data from each individual site
-		void PutDriverSites(int); // release gene drives into population
-		void UpdateConnec();// computes inter-patch connectivities
-		void OneStep(int);// controll population processes
-
-		/*--------- population processes (controlled from OneStep)-----*/
-		void JuvGetOlder();
-		void VirginsMate();
-		void AdultsMove();
-		void Hide();
-		void Wake(int);
-		void LayEggs();
-		void JuvEmerge();
-		void AdultsDie();
-		void UpdateComp(int);
-		void UpdateMate();
-		/*-------------------------------------------------------------------*/
-		void SetFertility();
-		void CheckCounts(int,char);// function for debugging - checks if totals are the same as the number in each class in each population
-
-		/*--------random number generater functions-----------------------*/
-		long long int random_poisson(double);
-		long long int random_binomial(long long int,double);
-		int IRandom(int, int);
-		double Random();
-		double random_normal(double,double);
-		std::vector<int> random_Multinomial(int n, const std::vector<double>& p);
-		/*-------------------------------------------------------------------*/
-
-		double distance(double,double,double,double,double);//distance between points (assuming toroidal boundaries)
-		double abso(double);//absolute used in distance function
+	double f[NumGen][NumGen][NumGen]; //f_ijk is fraction of type k offspring from mother with genotype i mated to father with genotype j
+};
 
 /*----------------------------------------------------------------------------------------*/
+
